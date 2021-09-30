2ed296f0e5d50625acc55d9806518900007e8a6b
Arun-Basnet <69413185+Arun-Basnet@users.noreply.github.com>
2021-09-30 19:51:52 +0600

    new update

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
-function compute()
+function simple_interest()
 {
-    p = document.getElementById("principal").value;
+    var p = document.getElementById("principal").value;
+    var r = document.getElementById("rate").value;
+    var t = document.getElementById("years").value;
+    var si = p * t * r/100;
+
+    var dateNow = new Date();
+    var yearNow = parseInt(dateNow.getFullYear()) + parseInt(t);
     
+    var resultDisplay = document.getElementById("result");
+    resultDisplay.innerHTML = "If you deposit " +  p  + ", <br> at an interest rate of " + r + "%." + "<br> You will receive an amount of " + si + ", <br> in the year " + yearNow;
 }
-        
+
+function sliderValue()
+{
+    var slider = document.getElementById("rate");
+    var output = document.getElementById("demo");
+    output.innerHTML = slider.value + "%"; // Display the default slider value
+
+    // Update the current slider value (each time you drag the slider handle)
+    slider.oninput = function() 
+    {
+        output.innerHTML = this.value + "%";
+    };
+}
